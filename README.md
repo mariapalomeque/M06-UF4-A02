fc7eb6b3d5069989eccaf222aaab39421244866e
mariapalomeque <2023_maria.palomeque@iticbcn.cat>
2025-03-27 23:17:41 +0100

    trabajo clase

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # M06-UF4-A02
+
+## Creación de el usuario
+
+Creamos el usuatio maria con su contraeña correspondiente, y le damos todos los privilegios, (lectura, escritura, ...) 
+![alt text](image.png)
---
+++ llibresapp/llibresapp/src/main/java/com/iticbcn/mywebapp/llibresapp/Controladors/BookController.java
@@ -0,0 +1,140 @@
+package com.iticbcn.mywebapp.llibresapp.Controladors;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+
+import com.iticbcn.mywebapp.llibresapp.Model.Llibre;
+import com.iticbcn.mywebapp.llibresapp.Model.Usuaris;
+import com.iticbcn.mywebapp.llibresapp.Repositoris.RepoLlibre;
+
+@Controller
+public class BookController {
+
+    @Autowired
+    RepoLlibre repoll = new RepoLlibre();
+
+
+    @GetMapping("/")
+    public String iniciar(Model Model){
+        return "login";
+    }
+
+ 
+
+    @PostMapping("/index")
+    public String login(@ModelAttribute("users") Usuaris users, Model model) {
+
+        model.addAttribute("users", users);
+
+        if (users.getUsuari().equals("pau") 
+        && users.getPassword().equals("1234")) {
+            return "index";
+        } else {
+            return "login";
+        }        
+    }
+
+        @GetMapping("/index")
+    public String index(@ModelAttribute("users") Usuaris users, Model model) {
+
+            return "index";
+        
+    }
+
+    @GetMapping("/consulta") 
+    public String consulta(@ModelAttribute("users") Usuaris users,Model model) {
+
+        ArrayList<Llibre> llibres = repoll.getAllLlibres();
+
+        model.addAttribute("llibres", llibres);
+        
+        return "consulta";
+    }
+
+    @GetMapping("/inserir") 
+    public String inputInserir(@ModelAttribute("users") Usuaris users,Model model) {
+        
+        return "inserir";
+    }
+    
+        @GetMapping("/cercaid")
+    public String inputCerca(@ModelAttribute("users") Usuaris users, Model model) {
+
+        Llibre llibre = new Llibre();
+        llibre.setIdLlibre(0);
+        model.addAttribute("llibreErr", true);
+        model.addAttribute("message", "");
+        model.addAttribute("llibre", llibre);
+
+        return "cercaid";
+
+    }
+
+    @PostMapping("/inserir")
+    public String inserir(@ModelAttribute("users") Usuaris users, Llibre llibre, Model model) {
+
+        repoll.InsertaLlibre(llibre);
+
+        ArrayList<Llibre> llibres = repoll.getAllLlibres();
+
+        model.addAttribute("llibres", llibres);
+        
+        return "consulta";
+    
+    }
+        @PostMapping("/cercaid")
+    public String cercaId(@ModelAttribute("users") Usuaris users,
+                          @RequestParam(name = "idLlibre", required = false) String idLlibre, 
+                          Model model) {
+        
+        int idLlib = 0;
+        String message = "";
+        boolean llibreErr = false;
+
+        try {
+            idLlib = Integer.parseInt(idLlibre);
+            Llibre llibre = repoll.getLlibreID(idLlib);
+            if(llibre !=null) {
+                model.addAttribute("llibre", llibre);
+            } else {
+                message = "No hi ha cap llibre amb aquesta id";
+                llibreErr = true;
+            }
+
+        } catch (Exception e) {
+            message = "La id de llibre ha de ser un nombre enter";
+            llibreErr = true;
+        } 
+        
+        model.addAttribute("message", message);
+        model.addAttribute("llibreErr",llibreErr);
+
+        return "cercaid";
+
+    }
+
+
+        @PostMapping("/logout")
+    public String logout(SessionStatus status) {
+        status.setComplete();
+        return "redirect:/";
+    }
+
+
+    @ModelAttribute("users")
+    public Usuaris getDefaultUser() {
+        return new Usuaris(); 
+    }
+
+
+
+
+}
